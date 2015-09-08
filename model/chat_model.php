17375fa3e65acb5d4c4c726d64b7795558db9efd
drinbigs <drinzbigs@gmail.com>
2015-09-08 16:03:49 +0800

    Change the CSS in chat box and fix bugs

--- model/chat_model.php
+++ model/chat_model.php
@@ -6,7 +6,7 @@ include 'session.php';
 	$query = mysqli_query($connection, $qry);
 	
 	if($query) {
-		echo "--> " .$login_session. " is now online";
+		echo "<i>" .$login_session. " is now online</i>";
 	if (mysqli_num_rows($query) > 0) {
     // output data of each row
     while($row = mysqli_fetch_assoc($query)) {
